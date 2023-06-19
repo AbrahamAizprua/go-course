9233a7ab745d60990c53b0fbc16b7adc52520eaf
Abraham Aizprua <abraham.aizprua@gmail.com>
2023-06-18 23:06:45 -0500

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aaizprua/go-course
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/aaizprua/go-course/variables"
+)
+
+func main() {
+	variables.MostrarEnteros()
+}
